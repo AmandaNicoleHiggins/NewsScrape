6c4faaa2881f3ee588b105626f09e54b2f825149
Amanda Higgins <amanda.n.higgins@gmail.com>
2020-05-01 16:13:42 -0400

    added .then to fetch/scrape

---
+++ controllers/headlines.js
@@ -0,0 +1,48 @@
+var scrape = require("../scripts/scrape");
+var makeDate = require("../scripts/date");
+
+var Headline = require("../models/Headline");
+
+module.exports = {
+  fetch: function(cb) {
+
+   return scrape()
+    .then(function(data){
+      console.log('DATA', data)
+        var articles = data;
+ 
+       for (var i = 0; i < articles.length; i++) {
+         articles[i].date = makeDate();
+         articles[i].saved = false;
+       }
+
+      Headline.collection.insertMany(articles, { ordered: false }, function(err, docs) {
+
+        console.log('cb', cb)
+      cb(err, docs);
+     });
+
+    });
+  },
+  delete: function(query, cb) {
+    Headline.remove(query, cb);
+  },
+  get: function(query, cb) {
+   
+    Headline.find(query)
+      .sort({
+        _id: -1
+      })
+
+      .exec(function(err, doc) {
+   
+        cb(doc);
+      });
+  },
+  update: function(query, cb) {
+ 
+    Headline.update({ _id: query._id }, {
+      $set: query
+    }, {}, cb);
+  }
+};
